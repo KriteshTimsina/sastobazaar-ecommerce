2bb0c022fb5f4b5ef3a3ca6c74349deeb5b1a8bf
redshirtfull456 <nishanphuyal111@gmail.com>
2023-10-14 12:58:25 +0545

    feat-validation

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -5,12 +5,71 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 const Checkout = () => {
   const { state: items } = useLocation();
   const [paymentMethod, setPaymentMethod] = useState("pay");
+  const [email, setEmail] = useState("");
+  const [cardHolder, setCardHolder] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardExpiry, setCardExpiry] = useState("");
+  const [cardCVC, setCardCVC] = useState("");
+  const [billingAddress, setBillingAddress] = useState("");
+  const [billingState, setBillingState] = useState("");
+  const [billingZip, setBillingZip] = useState("");
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = {};
+
+    if (!email || !email.trim()) {
+      newErrors.email = "Email is required";
+      valid = false;
+    }
+
+    if (!cardHolder || !cardHolder.trim()) {
+      newErrors.cardHolder = "Card Holder is required";
+      valid = false;
+    }
+    if (paymentMethod === "pay") {
+      if (!cardNumber || !/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(cardNumber)) {
+        newErrors.cardNumber = "Invalid card number format";
+        valid = false;
+      }
+
+      if (!cardExpiry || !/^\d{2}\/\d{2}$/.test(cardExpiry)) {
+        newErrors.cardExpiry = "Invalid expiry format (MM/YY)";
+        valid = false;
+      }
+
+      if (!cardCVC || !/^\d{3}$/.test(cardCVC)) {
+        newErrors.cardCVC = "Invalid CVC format";
+        valid = false;
+      }
+    }
+
+    if (!billingAddress || !billingAddress.trim()) {
+      newErrors.billingAddress = "Billing Address is required";
+      valid = false;
+    }
+
+    if (!billingState) {
+      newErrors.billingState = "Select a State";
+      valid = false;
+    }
+    if (!billingZip || !/^\d{5}$/.test(billingZip)) {
+      newErrors.billingZip = "Invalid ZIP code";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
 
   const placeOrder = (e) => {
     e.preventDefault();
-    navigate("/checkout/success");
+
+    if (validateForm()) navigate("/checkout/success");
   };
+
   return (
     <div className="pt-navtop dark:bg-darkbg">
       <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
@@ -103,15 +162,17 @@ const Checkout = () => {
             >
               Email
             </label>
-            <div className="relative ">
+            <div className="relative  flex">
               <input
                 required
                 type="text"
                 id="email"
                 name="email"
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="your.email@gmail.com"
               />
+
               <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -129,21 +190,26 @@ const Checkout = () => {
                 </svg>
               </div>
             </div>
+
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
             <label
               for="card-holder"
               className="mt-4 mb-2 block text-sm font-medium dark:text-white"
             >
               Card Holder
             </label>
-            <div className="relative">
+
+            <div className="relative flex">
               <input
                 required
                 type="text"
                 id="card-holder"
                 name="card-holder"
+                onChange={(e) => setCardHolder(e.target.value)}
                 className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                 placeholder="Your full name here"
               />
+
               <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -161,6 +227,10 @@ const Checkout = () => {
                 </svg>
               </div>
             </div>
+            {errors.cardHolder && (
+              <p className="text-red-500">{errors.cardHolder}</p>
+            )}
+
             {paymentMethod === "pay" ? (
               <>
                 <label
@@ -170,43 +240,65 @@ const Checkout = () => {
                   Card Details
                 </label>
                 <div className="flex">
-                  <div className="relative w-7/12 flex-shrink-0">
+                  <div>
+                    <div className="relative  flex-shrink-0">
+                      <input
+                        required
+                        type="text"
+                        id="card-no"
+                        name="card-no"
+                        onChange={(e) => setCardNumber(e.target.value)}
+                        className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="xxxx-xxxx-xxxx-xxxx"
+                      />
+
+                      <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                        <svg
+                          className="h-4 w-4 text-gray-400"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
+                          <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
+                        </svg>
+                      </div>
+                    </div>
+                    {errors.cardNumber && (
+                      <p className="text-red-500">{errors.cardNumber}</p>
+                    )}
+                  </div>
+
+                  <div>
                     <input
                       required
                       type="text"
-                      id="card-no"
-                      name="card-no"
-                      className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                      placeholder="xxxx-xxxx-xxxx-xxxx"
+                      name="credit-expiry"
+                      onChange={(e) => setCardExpiry(e.target.value)}
+                      className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                      placeholder="MM/YY"
                     />
-                    <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                      <svg
-                        className="h-4 w-4 text-gray-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
-                        <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
-                      </svg>
-                    </div>
+                    {errors.cardExpiry && (
+                      <p className="text-red-500"> {errors.cardExpiry}</p>
+                    )}
                   </div>
-                  <input
-                    required
-                    type="text"
-                    name="credit-expiry"
-                    className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="MM/YY"
-                  />
-                  <input
-                    required
-                    type="text"
-                    name="credit-cvc"
-                    className="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                    placeholder="CVC"
-                  />
+
+                  <div>
+                    <input
+                      required
+                      type="text"
+                      name="credit-cvc"
+                      onChange={(e) => setCardCVC(e.target.value)}
+                      className="w-full flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                      placeholder="CVC"
+                    />
+
+                    {errors.cardCVC && (
+                      <p className="text-red-500"> {errors.cardCVC}</p>
+                    )}
+                  </div>
                 </div>
               </>
             ) : null}
@@ -216,45 +308,68 @@ const Checkout = () => {
             >
               Address
             </label>
-            <div className="flex flex-col sm:flex-row">
-              <div className="relative flex-shrink-0 sm:w-7/12">
-                <input
+            <div className="flex flex-col sm:flex-row ">
+              <div>
+                <div className="relative flex flex-shrink-0 ">
+                  <input
+                    required
+                    type="text"
+                    id="billing-address"
+                    name="billing-address"
+                    onChange={(e) => setBillingAddress(e.target.value)}
+                    className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                    placeholder="District"
+                  />
+
+                  <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                    <img
+                      className="h-4 w-4 object-contain"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Flag_of_Nepal.svg/209px-Flag_of_Nepal.svg.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                {errors.billingAddress && (
+                  <p className="text-red-500"> {errors.billingAddress}</p>
+                )}
+              </div>
+
+              <div>
+                <select
                   required
                   type="text"
-                  id="billing-address"
-                  name="billing-address"
-                  className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                  placeholder="District"
-                />
-                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                  <img
-                    className="h-4 w-4 object-contain"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Flag_of_Nepal.svg/209px-Flag_of_Nepal.svg.png"
-                    alt=""
-                  />
-                </div>
+                  name="billing-state"
+                  onChange={(e) => setBillingState(e.target.value)}
+                  className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                >
+                  <option value="">Select State</option>
+                  <option value="1">Koshi Province</option>
+                  <option value="2">Madhesh Province</option>
+                  <option value="3">Janakpur Province</option>
+                  <option value="4">Bagmati Province</option>
+                  <option value="5">Lumbini Province</option>
+                  <option value="6">Gandaki Province</option>
+                  <option value="7">Sudur Paschim Province</option>
+                </select>
+
+                {errors.billingState && (
+                  <p className="text-red-500">{errors.billingState}</p>
+                )}
               </div>
-              <select
-                required
-                type="text"
-                name="billing-state"
-                className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-              >
-                <option value="">Select State</option>
-                <option value="1">Koshi Province</option>
-                <option value="2">Madhesh Province</option>
-                <option value="3">Janakpur Province</option>
-                <option value="4">Bagmati Province</option>
-                <option value="5">Lumbini Province</option>
-                <option value="6">Gandaki Province</option>
-                <option value="7">Sudur Paschim Province</option>
-              </select>
-              <input
-                type="text"
-                name="billing-zip"
-                className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                placeholder="ZIP"
-              />
+
+              <div>
+                <input
+                  type="text"
+                  name="billing-zip"
+                  onChange={(e) => setBillingZip(e.target.value)}
+                  className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/2 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="ZIP"
+                />
+
+                {errors.billingZip && (
+                  <p className="text-red-500"> {errors.billingZip}</p>
+                )}
+              </div>
             </div>
 
             <div className="mt-6 border-t border-b py-2">
@@ -296,5 +411,4 @@ const Checkout = () => {
     </div>
   );
 };
-
 export default Checkout;
