b852999f5c5ec608b53812b7ceda09264f8d8b31
Itezaz ul Hassan <109591867+Itezaz-ul-Hassan@users.noreply.github.com>
2023-10-14 17:27:02 +0000

    Change param type for function

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,7 @@
 /**
  * This function is to convert USD to NPR using the current dollar rate
  *
- * @param {Array} amount - A number/string.
+ * @param {number} amount - A number/string.
  * @returns {number}
  */
 export function getLocalPrice(amount, dollarRate=100) {
