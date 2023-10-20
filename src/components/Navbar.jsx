d662de3c80ec321b912e30cf3f78cf8d2120bd98
RameezIbrahimOfficial <rameezibrahimofficial@gmail.com>
2023-10-20 13:40:50 +0530

    Made mobile navbar look better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import { useTheme } from "../context/ThemeContext";
 import { useAuth0 } from "@auth0/auth0-react";
 import Userdetails from "./auth/Userdetails";
+import { FaShoppingCart } from "react-icons/fa"; // Import cart icon
 
 const Navbar = ({
   isNavbarExtended,
@@ -19,9 +20,23 @@ const Navbar = ({
     setToggleProfile(!toggleProfile);
   };
 
+  const toggleHamburgerMenu = () => {
+    setIsNavbarExtended(!isNavbarExtended);
+  };
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-30 bg-gray-900 border-gray-200 shadow-sm shadow-white">
-      <div className="flex flex-wrap items-center justify-start max-w-screen-xl p-4 mx-auto md:justify-around">
+      <div className="flex flex-wrap items-center justify-start max-w-screen-xl pt-3 pl-0 pb-3 mx-auto md:justify-around">
+      <button
+            onClick={toggleHamburgerMenu}
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="relative inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <RxHamburgerMenu size={25} />
+          </button>
         <Link
           to="/"
           className="flex items-center self-center text-2xl font-semibold text-white uppercase"
@@ -31,51 +46,41 @@ const Navbar = ({
         </Link>
 
         <div className="absolute flex items-center gap-3 right-5">
-          <button
-            onClick={() => setIsNavbarExtended(!isNavbarExtended)}
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="relative inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <RxHamburgerMenu size={25} />
-            {isNavbarExtended && (
-              <div
-                className="z-10 absolute top-[58px]
-           w-[150px] h-[125px] -translate-x-1/2 bg-gray-900 flex flex-col items-center gap-1"
+          <DarkModeSwitch
+            checked={isDarkTheme}
+            onChange={toggleTheme}
+            className="text-yellow-400 dark:text-gray-500"
+          />
+        
+          {isNavbarExtended && (
+            <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 flex flex-col items-center justify-center">
+              <button
+                onClick={toggleHamburgerMenu}
+                className="absolute top-4 right-4 text-white text-2xl cursor-pointer"
               >
-                <Link
-                  to="/"
-                  className="p-1 hover:bg-slate-700 hover:text-blue-50 "
-                >
+                &#10006;{/* Close button (X) */}
+              </button>
+              <div className="flex flex-col items-center gap-2">
+                <Link to="/" className="text-white">
                   Home
                 </Link>
-                <Link
-                  to="/product"
-                  className="p-1 hover:bg-slate-700 hover:text-blue-500 "
-                >
+                <Link to="/product" className="text-white">
                   Products
                 </Link>
-                <Link
-                  to="/cart"
-                  className="p-1 hover:bg-slate-700 hover:text-blue-500 "
-                >
+                <div className="text-white cursor-pointer">
                   Cart
-                </Link>
-                <div>
-                  {!isAuthenticated && (
-                    <input
-                      type="button"
-                      value="Login"
-                      className="p-1 cursor-pointer hover:bg-slate-700 hover:text-blue-500"
-                      onClick={() => loginWithRedirect()}
-                    />
-                  )}
                 </div>
+                {!isAuthenticated && (
+                  <button
+                    onClick={() => loginWithRedirect()}
+                    className="text-white"
+                  >
+                    Login
+                  </button>
+                )}
               </div>
-            )}
-          </button>
+            </div>
+          )}
           <div className="md:hidden">
             {isAuthenticated && (
               <Userdetails
@@ -83,14 +88,9 @@ const Navbar = ({
               />
             )}
           </div>
-          <DarkModeSwitch
-            checked={isDarkTheme}
-            onChange={toggleTheme}
-            className="text-yellow-400 dark:text-gray-500"
-          />
         </div>
         <div className="hidden w-full md:block md:w-auto " id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark-bg-gray-900 dark:border-gray-700">
             <li>
               <NavLink
                 to="/"
@@ -113,26 +113,25 @@ const Navbar = ({
                 Products
               </NavLink>
             </li>
-
             <li>
               <NavLink
                 style={({ isActive }) => ({
                   color: isActive ? "#3b82f6" : "white",
                 })}
                 to="/cart"
-                className="block py-2 pl-3 pr-4 rounded text-w hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 rounded text-w hover:bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark:text-white md:dark-text-blue-500 dark-bg-gray-700 dark-text-white md:dark-bg-transparent"
               >
                 Cart
               </NavLink>
             </li>
             <li>
               {!isAuthenticated ? (
-                <input
-                  type="button"
-                  value="Login"
-                  className="text-white cursor-pointer"
+                <button
                   onClick={() => loginWithRedirect()}
-                />
+                  className="text-white cursor-pointer"
+                >
+                  Login
+                </button>
               ) : (
                 <Userdetails
                   {...{ user, logout, toggleProfile, showUserDetails }}
