48de7d269fa8dd58e1ff41f1530a0a48e5637f99
RameezIbrahimOfficial <rameezibrahimofficial@gmail.com>
2023-10-20 22:58:58 +0530

    ✅ Removed background from navbar in large screen when dark mode is on ✅ In mobile, clicking on links should go to that page and also close the sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 import { useTheme } from "../context/ThemeContext";
 import { useAuth0 } from "@auth0/auth0-react";
 import Userdetails from "./auth/Userdetails";
-import { FaShoppingCart } from "react-icons/fa"; // Import cart icon
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = ({
   isNavbarExtended,
@@ -24,6 +24,12 @@ const Navbar = ({
     setIsNavbarExtended(!isNavbarExtended);
   };
 
+  const closeHamburgerMenu = () => {
+    if (isNavbarExtended) {
+      toggleHamburgerMenu();
+    }
+  };
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-30 bg-gray-900 border-gray-200 shadow-sm shadow-white">
       <div className="flex flex-wrap items-center justify-start max-w-screen-xl pt-3 pl-0 pb-3 mx-auto md:justify-around">
@@ -31,7 +37,7 @@ const Navbar = ({
             onClick={toggleHamburgerMenu}
             data-collapse-toggle="navbar-default"
             type="button"
-            className="relative inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="relative inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover-bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover-bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
           >
@@ -40,9 +46,10 @@ const Navbar = ({
         <Link
           to="/"
           className="flex items-center self-center text-2xl font-semibold text-white uppercase"
+          onClick={closeHamburgerMenu} // Close hamburger menu when Home is clicked
         >
           Sasto&nbsp;
-          <span className=" whitespace-nowrap text-primary"> Bazaar 🛒</span>
+          <span className="whitespace-nowrap text-primary"> Bazaar 🛒</span>
         </Link>
 
         <div className="absolute flex items-center gap-3 right-5">
@@ -61,18 +68,21 @@ const Navbar = ({
                 &#10006;{/* Close button (X) */}
               </button>
               <div className="flex flex-col items-center gap-2">
-                <Link to="/" className="text-white">
+                <Link to="/" className="text-white" onClick={closeHamburgerMenu}>
                   Home
                 </Link>
-                <Link to="/product" className="text-white">
+                <Link to="/product" className="text-white" onClick={closeHamburgerMenu}>
                   Products
                 </Link>
-                <div className="text-white cursor-pointer">
+                <Link to="/cart" className="text-white cursor-pointer" onClick={closeHamburgerMenu}>
                   Cart
-                </div>
+                </Link>
                 {!isAuthenticated && (
                   <button
-                    onClick={() => loginWithRedirect()}
+                    onClick={() => {
+                      loginWithRedirect();
+                      closeHamburgerMenu();
+                    }}
                     className="text-white"
                   >
                     Login
@@ -90,7 +100,7 @@ const Navbar = ({
           </div>
         </div>
         <div className="hidden w-full md:block md:w-auto " id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark-bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-900 md:dark-bg-gray-900 dark:border-gray-700">
             <li>
               <NavLink
                 to="/"
@@ -98,6 +108,7 @@ const Navbar = ({
                   color: isActive ? "#3b82f6" : "white",
                 })}
                 className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 "
+                onClick={closeHamburgerMenu} // Close hamburger menu when Home is clicked
               >
                 Home
               </NavLink>
@@ -109,6 +120,7 @@ const Navbar = ({
                   color: isActive ? "#3b82f6" : "white",
                 })}
                 className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 "
+                onClick={closeHamburgerMenu} // Close hamburger menu when Products is clicked
               >
                 Products
               </NavLink>
@@ -119,7 +131,8 @@ const Navbar = ({
                   color: isActive ? "#3b82f6" : "white",
                 })}
                 to="/cart"
-                className="block py-2 pl-3 pr-4 rounded text-w hover:bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark:text-white md:dark-text-blue-500 dark-bg-gray-700 dark-text-white md:dark-bg-transparent"
+                className="block py-2 pl-3 pr-4 rounded text-w md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark:text-white md:dark-text-blue-500 dark-bg-gray-700 dark-text-white md:dark-bg-transparent"
+                onClick={closeHamburgerMenu} // Close hamburger menu when Cart is clicked
               >
                 Cart
               </NavLink>
@@ -127,7 +140,10 @@ const Navbar = ({
             <li>
               {!isAuthenticated ? (
                 <button
-                  onClick={() => loginWithRedirect()}
+                  onClick={() => {
+                    loginWithRedirect();
+                    closeHamburgerMenu();
+                  }}
                   className="text-white cursor-pointer"
                 >
                   Login
