fc871ed1f26d7b1efe58a37418f9d5fcbf9a277e
BuilderPrid <priyanshudwivedi004@gmail.com>
2023-10-24 15:49:55 +0530

    wishlist feature added with favourite/unfavourite options

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AiFillHeart, AiOutlineEye } from "react-icons/ai";
+import { AiFillAmazonCircle, AiFillHeart, AiOutlineEye, AiOutlineHeart } from "react-icons/ai";
 import Rating from "./Rating";
 import { cartContext } from "../context/cartContext";
 import { getLocalPrice } from "../utils/common";
 
-const Card = ({ products }) => {
+const Card = ({ products, onRemoveWishlist }) => {
   const navigate = useNavigate();
+  const [wishlist, setwishlist] = useState(JSON.parse(localStorage?.getItem("wishlist")) || []);
 
+  console.log(wishlist, 'wl')
   function handleProductClick(id) {
     navigate(`/product/${id}`);
+  }
+  function handleWishlistClick(product) {
+    console.log("click", product);
+    const index = wishlist.find(item => item.id === product.id) !== undefined;
+    console.log(index)
+    if (!index) {
+      const confirmation = confirm("Do you want to add this item to wishlist ?");
+      if (confirmation) {
+        setwishlist((prev) => {
+          return [...prev, product];
+        });
+        localStorage.setItem("wishlist", JSON.stringify([...wishlist, product]));
+      }
+    }
+    else {
+      const confirmation = confirm("Do you want to remove this item?");
+      if (confirmation) {
+        const updatedWishlist = wishlist.filter((item) => item.id !== product.id);
+        setwishlist(updatedWishlist);
+        console.log(updatedWishlist, "updated")
+        if (onRemoveWishlist) onRemoveWishlist(product.id);
+        localStorage.setItem("wishlist", JSON.stringify([...updatedWishlist]))
+      }
+    }
   }
 
   return (
@@ -48,7 +74,13 @@ const Card = ({ products }) => {
               className="absolute -right-5 top-0 cursor-pointer
               bg-pink-300 text-white p-1 text-3xl flex flex-col items-center gap-2 opacity-0 group-hover:opacity-100 group-hover:right-0 transition-all "
             >
-              <AiFillHeart title="Favourite" className="text-red-500" />
+              {wishlist.find(item => item.id === product.id) !== undefined ? <AiOutlineHeart
+                onClick={() => handleWishlistClick(product)}
+                title="Un-Favourite" className="text-red-500" /> :
+                <AiFillHeart
+                  onClick={() => handleWishlistClick(product)}
+                  title="Favourite" className="text-red-500" />}
+              {console.log(products.find(item => item.id === product.id), 'g')}
               <div className="bg-primary p-1 text-white">
                 <AiOutlineEye
                   onClick={() => handleProductClick(product.id)}
