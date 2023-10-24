d19e31cdc99f6d30d9c1d6cfb269815c26421d00
12345rams <132057037+12345rams@users.noreply.github.com>
2023-10-24 09:06:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import Checkout from "./pages/checkout/Checkout";
 import Success from "./pages/checkout/Success";
 import Error from "./pages/404/Error";
 import Footer from "./components/Footer";
+import FAQ from "./components/FAQ";
 function App() {
   const [isNavbarExtended, setIsNavbarExtended] = useState(false);
   const [toggleProfile, setToggleProfile] = useState(false);
@@ -56,6 +57,7 @@ function App() {
               }
             />
             <Route path="/checkout" element={<Checkout />} />
+              <Route path="/FAQ" element={<FAQ />} />
             <Route path="/checkout/success" element={<Success />} />
             <Route path="*" element={<Error />} />
           </Routes>
