5319e8410da94a40902ee788edca0ee3b448304b
kriteshtimsina <kriteshtimsina55@gmail.com>
2025-03-31 19:11:22 +0545

    feat(b): refactor category model

--- backend/src/controllers/ProductCategoryController.ts
+++ backend/src/controllers/ProductCategoryController.ts
@@ -1,35 +1,44 @@
 import expressAsyncHandler from "express-async-handler";
 import { ProductCategory } from "../models/productCategoryModel";
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 
-export const createCategory = expressAsyncHandler(async(req:Request,res:Response)=>{
+export const createCategory = expressAsyncHandler(
+  async (req: Request, res: Response) => {
     try {
-        const category  = await ProductCategory.create(req.body);
-        if(category){
-            res.status(201).json({
-                status:true,
-                message:"Category created",
-                category
-            })
-        }
-    } catch (error:any) {
-        throw new Error(error.message)
+      const category = await ProductCategory.create(req.body);
+      if (category) {
+        res.status(201).json({
+          status: true,
+          message: "Category created",
+          category,
+        });
+      }
+    } catch (error: any) {
+      throw new Error(error.message);
     }
+  }
+);
 
-})
-
-export const getAllCategories = expressAsyncHandler(async(req:Request,res:Response)=>{
+export const getAllCategories = expressAsyncHandler(
+  async (req: Request, res: Response) => {
     try {
-        const category = await ProductCategory.find({});
-        res.status(200).json({
-            status:true,
-            message:"Categories found",
-            category
-        })
+      const category = await ProductCategory.find({});
+      res.status(200).json({
+        status: true,
+        message: "Categories found",
+        category,
+      });
     } catch (error) {
-        throw new Error(error.message)
+      throw new Error(error.message);
     }
-})
-export const editCategory = expressAsyncHandler(async(req:Request,res:Response)=>{})
-export const deleteCategory = expressAsyncHandler(async(req:Request,res:Response)=>{})
-export const getSingleCategory = expressAsyncHandler(async(req:Request,res:Response)=>{})
+  }
+);
+export const editCategory = expressAsyncHandler(
+  async (req: Request, res: Response) => {}
+);
+export const deleteCategory = expressAsyncHandler(
+  async (req: Request, res: Response) => {}
+);
+export const getSingleCategory = expressAsyncHandler(
+  async (req: Request, res: Response) => {}
+);
